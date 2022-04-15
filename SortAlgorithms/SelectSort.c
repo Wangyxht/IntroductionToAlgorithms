806a8801ff45e8fccffb283d76c89b1a46fc6e07
Wangyxht <wangyx0001@qq.com>
2022-04-15 16:52:03 +0800

    选择排序

--- SortAlgorithms/SelectSort.c
+++ SortAlgorithms/SelectSort.c
@@ -2,11 +2,12 @@
 #include<stdlib.h>
 #define SIZE 10
 
+//选择排序
 int* SelectSort(int*,int);
 
 int main(void)
 {
-    int test[SIZE]={23,4,67,89,23,56,43,78,34,8};
+    int test[SIZE]={23,4,67,89,90,56,43,78,34,8};
     int i;
 
     for(i=0;i<SIZE;i++)
@@ -37,19 +38,21 @@ int* SelectSort(int* array,int arraySize)
 
     for(i=0;i<arraySize-1;i++)
     {
+
+        temp=i;
         for(j=i+1,minNumber=array[i];j<arraySize;j++)
         {
             if(array[j]<minNumber)
             {
-                minNumber=array[j];
-                temp=j;
+                minNumber=array[j];//对于从数组第i+1的值到第n个值，寻找此区间的最小值
+                temp=j;//存储下标
             }
 
         }
-        if(temp!=i)
+        if(temp!=i)//如果最小值不是数组第i个值
         {
             array[temp]=array[i];
-            array[i]=minNumber;
+            array[i]=minNumber;//交换第i个值与最小值的次序.
         }    
     }
 
